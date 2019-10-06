f97199b861f68b07bda89f4b5c5341d0d624fe51
yungdenzel <oyomafia@gmail.com>
2019-10-06 15:27:08 +0100

    changes to the styling and js

--- js/index.js
+++ js/index.js
@@ -3,38 +3,41 @@ let score = 0;
 let questionIndex = 0;
 
 const gradeQuestion = (ans) => {
-  // get value on selected option 
-  // check if correct
-  // add mark to score
+ 
   if (ans == selectQuestions[questionIndex].answer) {
     score += 20;
   }
-  // increament questionIndex by 1 questionIndex ++
+  
   questionIndex += 1;
 };
 
 const renderSuccess = () => {
-    $('.question-number').text('');
+  $('.question-number').text('');
   $('.question-question').text('');
   $('.option1').text('');
   $('.option2').text('');
   $('.option3').text('');
   $('.option4').text('');
+  $(`h1`).remove();
+  let points = $(`<span class="score" ></span>`);
+
   if (score >= 50) {
-    $('<p>You are God of Football!</p>').appendTo('.question-question');
-    $('<img src="https://media.giphy.com/media/xT8qBoFt9PszavNyeI/giphy.gif">').appendTo('.question-options');
+    points.text(`Score : ${score}%`).appendTo('.question-question');
+    $('<p>You do know football</p>').appendTo('.question-question');
+    $('<img class="animated-gif" src="https://media.giphy.com/media/xT8qBoFt9PszavNyeI/giphy.gif">').appendTo('.question-options');
   } else {
-    $('<p>you can do better!</p>').appendTo('.question-question');
-    $('<img src="https://media.giphy.com/media/uO8Mn35ytbHm8/giphy.gif">').appendTo('.question-options');
+    points.text(`Score: ${score}%`).appendTo('.question-question');
+    $( '<p>You can do better!</p>').appendTo('.question-question');
+    $('<img class="animated-gif" src="https://media.giphy.com/media/uO8Mn35ytbHm8/giphy.gif">').appendTo('.question-options');
   }
 
 
 }
 
-// call function to render question (questionIndex)
+
 const renderQuestion = (qIndex) => {
   let question = selectQuestions[qIndex];
-  // get question number and populate qith question number
+  // get question number and populate with question number
   $('.question-number').text('');
   $('.question-question').text('');
   $('.option1').text('');
@@ -44,15 +47,15 @@ const renderQuestion = (qIndex) => {
   $('.question-number').text('Question ' + (qIndex + 1));
   // get question-question and populate with quesion
   $('.question-question').text(question.question);
-  // get question-options and populate with options as radio
+  // get question-options and populate with options as button
   question.options.forEach((element, i) => {
     let opt = '.option' + (i + 1);
-    let radioBut = $('<input type="radio" name="answer" value=' + element + ' />');
-    let radioLabel = $('<label for=' + element + ' >' + element + ' </label>');
-    radioBut.appendTo(opt);
-    radioLabel.appendTo(opt);
+    let buttonBut = $('<input type="button" class="test" name="answer"  value=' + element + ' />');
+    let buttonLabel = $('<label for=' +element+ '>'  +'</label>');
+    buttonBut.appendTo(opt);
+    buttonLabel.appendTo(opt);
   });
-  $(".question-options input:radio[name='answer']").click(function () {
+  $(".question-options input:button[name='answer']").click(function () {
     if (questionIndex == (selectQuestions.length - 1)) {
       gradeQuestion(this.value)
       renderSuccess()
@@ -63,7 +66,6 @@ const renderQuestion = (qIndex) => {
   });
 };
 
-// on click on options run function gradeQuestion(e)
 
 
 $(document).ready(function () {
--- js/question.js
+++ js/question.js
@@ -1,8 +1,8 @@
 const qAndA = [
   {
       question: "Which is the odd one out?",
-      options:  ["Liverpool", "Chelsea","Real Madrid", "Arsenal"],
-      answer: "Real Madrid"
+      options:  ["Liverpool", "Chelsea","Ajax", "Arsenal"],
+        answer: "Ajax"
   },
   {
       question: "Which football team plays its home games at Wembley?",
@@ -10,12 +10,7 @@ const qAndA = [
       answer: "England"
   },
 
-  {
-      question: "Who managed Manchester City between June 2013 and June 2016?",
-      options: ["Mourinho", "Manuel Pellegrini","Carlos Ancelloti", "Arsene Wenger"],
-      answer: "Manuel Pellegrini",
-  },
-    {
+   {
         question: "Who won the 1994 FIFA World Cup?",
         options: ["Nigeria", "England", "Egypt","Brazil"],
         answer: "Brazil",
@@ -23,7 +18,7 @@ const qAndA = [
 
     {
         question: "Which of these star soccer players has never played for Real Madrid?",
-        options: ["Messi", "C. Ronaldo", "Hazard", "Benzema"],
+        options: ["Messi", "Ronaldo", "Hazard", "Benzema"],
         answer: "Messi",
     },
 
@@ -34,23 +29,23 @@ const qAndA = [
     },
     {
         question: "Which country will host the 2022 World Cup?",
-        options: ["South Africa", "Kenya", "Nigeria", "Qatar"],
+        options: ["England", "Kenya", "Nigeria", "Qatar"],
         answer: "Qatar",
     },
 
     {
         question: "Who won the World Cup in 2014?",
-        options: ["South Africa", "Germany", "Argentina", "Canada"],
+        options: ["Brazil", "Germany", "Argentina", "Canada"],
         answer: "Germany",
     },
     {
         question: "Which country won the first World Cup in 1930?",
-        options: ["Uruguay", "Kenya", "Slovakia", "West Germany"],
+        options: ["Uruguay", "Kenya", "Slovakia", "Germany"],
         answer: "Uruguay",
     },
     {
         question: "Who is the winner of the 2018/2019 UEFA Champions League?",
-        options: ["Arsenal", "Real Madrid", "Barcelona", "Liverpool"],
+        options: ["Arsenal", "Chelsea", "Barcelona", "Liverpool"],
         answer: "Liverpool",
     },
 ]
